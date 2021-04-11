fd0ed5033f09f5d6a13b52acd61b37f261311ac3
Roaringman <rgengell@gmail.com>
2021-04-11 20:46:00 +0200

    Update FitToGeoJson.py
    1. Exchanged recursive function for loop.
    2. Created datatype checks for record data
    3. Created call from main function
    4. Added user input for directory path

--- Python/FitToGeoJson.py
+++ Python/FitToGeoJson.py
@@ -1,61 +1,85 @@
 from datetime import datetime
 startTime = datetime.now()
 from fitparse import *
-
-fitfile = FitFile('run06-05.FIT')
+import os
 
 def semicircleToDegrees(semicircles):
     return semicircles * ( 180 / 2**31 )
 
-def parseFitFile(file):
+def parseFitFile(fitfile):
     #Variable to hold the key for every entry in the dictionary
     point = 1
     dataDict = {}
     for record in fitfile.get_messages('record'):
         #List will hold values for each key in dictionary
         dataList = []
-        
         # Go through all the data entries in this record    
         for record_data in record:
-            if (record_data.name == 'speed'or record_data.name == 'distance'):
-                        
+            
+            if ((record_data.name == 'speed' or record_data.name == 'distance') and type(record_data.value) == float):  
                  dataList.append(record_data.value)
-                 
-            if (record_data.name == 'position_long' or record_data.name == 'position_lat'):
-                #Values provdied from the fitfiles have lat, lon in semicircles
+
+            if ((record_data.name == 'position_long' or record_data.name == 'position_lat') and type(record_data.value) == int):
+                #Values provdied from the fitfiles have lat, lon in semicircles (int)
                 dataList.append(semicircleToDegrees(record_data.value))
 
             if (record_data.name == 'timestamp'):
                 t = record_data.value
                 time = t.strftime('%H-%M-%S')
                 dataList.append(time)
-                
-        dataDict[point] = dataList
-        point += 1
+        #Only add record of all five datapoints are present        
+        if (len(dataList)==5):   
+            dataDict[point] = dataList
+            point += 1
     return dataDict
 
-def recursiveString(i):
-    inputlist = dataDict[i]
-    inputlist2 = dataDict[i+1]
-    pos1 = inputlist[1:3]
-    pos2 = inputlist2[1:3]
-    #Reverse coordinates to get lat first
-    pos1.reverse()
-    pos2.reverse()
-    prop1 = str(inputlist[0])
-    stringformat = ('{"type": "Feature","geometry":{"type":"LineString","coordinates":[%s,%s]},"properties":{"distance": "%s"}}' % (pos1, pos2, prop1))
+def recursiveString(dataDict):
+    stringformat = ""
+    stringList = []
+    i = 1
+    while i < len(dataDict) - 1:        
+        inputlist = dataDict[i]
+        inputlist2 = dataDict[i+1]
+        pos1 = inputlist[1:3]
+        pos2 = inputlist2[1:3]
+        #Reverse coordinates to get lat first
+        pos1.reverse()
+        pos2.reverse()
+        cumulativeDist = inputlist[0]
+        speed = inputlist[3]
+        stringformat = ('{"type": "Feature","geometry":{"type":"LineString","coordinates":[%s,%s]},"properties":{"distance": %s, "speed":%s}},' % (pos1, pos2, cumulativeDist, speed)) 
+        stringList.append(stringformat)
+        i = i + 1
+    return stringList
 
-    if (i == len(dataDict) - 1):
-       return stringformat
+
+def main():
+    userPath = input("Enter the path for a directory containing .FIT files: ") 
+    if(os.path.exists(userPath)):
+        fitFileList=[]
+        for filename in os.listdir(userPath):
+            if filename.endswith(".FIT"):
+                fitFileList.append(filename)
+
+        if(len(fitFileList) > 0):
+            try:
+                for fitfile in fitFileList:
+                    fit = FitFile(fitfile)
+                    dataDict = parseFitFile(fit)
+                    geojsonStart = '{"type": "FeatureCollection", "features": ['
+                    geojsonMid = ''.join([str(elem) for elem in recursiveString(dataDict)]) #Must start at one -> first key name in dictionary
+                    geojsonEnd = ']}'
+
+                    print(geojsonStart + geojsonMid[:-1] + geojsonEnd, file=open(fitfile + ".geojson", "w"))
+                    print(datetime.now() - startTime)
+            except:
+                print("Something unexpected happened while running the script.")
+                
+        else:
+            print("No .FIT files found in directory")
+        
     else:
-        string = stringformat + ','
-        return string + recursiveString(i+1)
+        print("Enter a valid input directory")
 
-dataDict = parseFitFile(fitfile)
-geojsonStart = '{"type": "FeatureCollection", "features": ['
-geojsonMid = recursiveString(1) #Must start at one -> first key name in dictionary
-geojsonEnd = ']}'
-
-print(geojsonStart + geojsonMid + geojsonEnd, file=open("run_GeoJson.geojson", "w"))
- 
-print(datetime.now() - startTime)
+if __name__ == "__main__":
+    main()
